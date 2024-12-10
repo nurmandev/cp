5475c6a11bf870b96afcfcc93f2f9c77506c575e
nurmandev <adebayour66265@gmail.com>
2024-12-10 10:57:12 +0100

    first commit

--- src/app/sections/howItWorks/section.tsx
+++ src/app/sections/howItWorks/section.tsx
@@ -1,43 +1,63 @@
-"use client";
-
 import Image, { StaticImageData } from "next/image";
 
-interface SectionProps {
+interface Props {
   id: number;
   title: string;
   description: string;
-  icon: StaticImageData; // Update to accept StaticImageData
+  icon?: StaticImageData | string; // Allow both static imports and strings
+  buttonText?: string;
   reverse?: boolean;
 }
 
-const Section: React.FC<SectionProps> = ({
+function Section({
   id,
   title,
   description,
   icon,
-  reverse,
-}) => {
+  buttonText,
+  reverse = false,
+}: Props) {
   return (
     <div
-      className={`flex flex-col md:flex-row items-center ${
-        reverse ? "md:flex-row-reverse" : ""
-      } gap-4 md:gap-8`}
+      className={`relative flex items-start gap-12 mb-10 ${
+        reverse
+          ? "md:justify-self-end flex-row-reverse -mr-[5.6rem]"
+          : "md:justify-self-start -ml-[5.6rem]"
+      }`}
     >
-      <div className="flex-shrink-0">
-        <Image
-          src={icon}
-          alt={`${title} icon`}
-          width={50}
-          height={50}
-          className="rounded"
-        />
+      <div className="flex-shrink-0 bg-yellow-400 w-10 h-10 flex items-center justify-center rounded-full text-black font-bold">
+        {id}
       </div>
       <div>
-        <h3 className="text-xl md:text-2xl font-semibold mb-2">{title}</h3>
-        <p className="text-gray-300">{description}</p>
+        <div
+          className={`flex items-center gap-4 mb-4 ${
+            reverse ? "flex-row-reverse" : "justify-start"
+          }`}
+        >
+          {icon && typeof icon === "string" ? (
+            <img src={icon} className="w-10" alt={`${title} icon`} />
+          ) : icon ? (
+            <Image
+              src={icon}
+              alt={`${title} icon`}
+              className="w-10"
+              width={40}
+              height={40}
+            />
+          ) : null}
+          <h3 className="text-2xl font-medium">{title}</h3>
+        </div>
+        <p className={`text-gray-400 ${reverse ? "text-right" : "text-left"}`}>
+          {description}
+        </p>
+        {buttonText && (
+          <button className="px-4 py-1 font-medium bg-yellow-400 text-black rounded-full hover:bg-yellow-500 mt-8">
+            {buttonText}
+          </button>
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default Section;
